780e97d599214c8c8e353cfa775c721c974d9b82
Maximov Danila <mr.quze@gmail.com>
2019-08-06 18:33:51 +0300

    fix(project): make it launch with no errors :S

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+// import React from "react"
+// import { useStaticQuery, graphql } from "gatsby"
+// import Img from "gatsby-image"
 
-/*
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- */
+// /*
+//  * For more information, see the docs:
+//  * - `gatsby-image`: https://gatsby.dev/gatsby-image
+//  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+// const Image = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+//         childImageSharp {
+//           fluid(maxWidth: 300) {
+//             ...GatsbyImageSharpFluid
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+//   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+// }
 
-export default Image
+// export default Image
