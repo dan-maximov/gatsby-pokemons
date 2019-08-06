7554094b3d8a22d7ade476ca847aa5ef4bb010d2
Maximov Danila <mr.quze@gmail.com>
2019-08-06 22:25:29 +0300

    fix: rm useless deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
