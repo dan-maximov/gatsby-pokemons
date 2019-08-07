162517eaf3804711fd63ee17f51e766060c62c00
Maximov Danila <mr.quze@gmail.com>
2019-08-07 20:28:44 +0300

    feat(PokemonCard): get rid of object-fit

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -44,10 +44,10 @@ const ImageWrapper = styled.div`
 
 const Image = styled.img`
   position: absolute;
-  /* TODO: ie compatible */
-  object-fit: scale-down;
-  height: 90%;
-  width: 90%;
+  max-height: 90%;
+  max-width: 90%;
+  height: auto;
+  width: auto;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
