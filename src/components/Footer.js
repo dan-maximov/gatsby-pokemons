8a63dd0ad98e577ea1948d5cffe11b99fb50548d
Danila Maximov <mr.quze@gmail.com>
2019-08-08 20:00:40 +0300

    feat(Footer): add social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { FaEnvelope, FaTwitter, FaGithub } from 'react-icons/fa';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const icon = css`
+  margin-left: 6px;
+`;
 
 const Wrapper = styled.footer`
   width: 100%;
@@ -11,30 +16,39 @@ const Wrapper = styled.footer`
 const Inner = styled.div`
   display: flex;
   align-items: center;
-  justify-content: flex-end;
   margin: 0 auto;
   max-width: 960px;
-  padding: 6px;
+  padding: 6px 6px 6px 0;
 `;
 
 const CurrentYear = styled.span`
+  margin-left: auto;
   margin-right: 3px;
+`;
+
+const IconLink = styled.a`
+  display: flex;
+  color: #fff;
 `;
 
 const Link = styled.a`
   color: #fff;
-  text-decoration: none;
-
-  &:hover {
-    text-decoration: underline;
-  }
 `;
 
 const Footer = () => (
   <Wrapper>
     <Inner>
+      <IconLink href="mailto:mr.quze@gmail.com">
+        <FaEnvelope css={icon} />
+      </IconLink>
+      <IconLink href="https://twitter.com/MaximovDanila" target="_blank" rel="noopener noreferrer">
+        <FaTwitter css={icon} />
+      </IconLink>
+      <IconLink href="https://github.com/meatspincom" target="_blank" rel="noopener noreferrer">
+        <FaGithub css={icon} />
+      </IconLink>
       <CurrentYear>{`© ${new Date().getFullYear()}, `}</CurrentYear>
-      <Link href="https://github.com/meatspincom" target="_blank" rel="noopener noreferrer">
+      <Link href="https://github.com/meatspincom/gatsby-pokemons" target="_blank" rel="noopener noreferrer">
         meatspincom
       </Link>
     </Inner>
