094a3d00b231a55d6b0dc2cbe585d92148f5613e
Danila Maximov <mr.quze@gmail.com>
2019-08-08 18:06:29 +0300

    feat: custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,35 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import SEO from '../components/Seo';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px 16px 12px;
+
+  h2 {
+    margin-bottom: 12px;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  margin: 0 auto;
+  img {
+    width: auto;
+    height: auto;
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
 const NotFoundPage = () => (
-  <>
+  <Wrapper>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
+    <h2>Page was not found</h2>
+    <ImageWrapper>
+      <img src="./404.jpg" alt="Snivy" />
+    </ImageWrapper>
+  </Wrapper>
 );
 
 export default NotFoundPage;
