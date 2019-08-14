45812f2db14dd467c774e47e984ace83b003ecd7
Danila Maximov <mr.quze@gmail.com>
2019-08-14 03:37:17 +0300

    feat(LazyImage): dont flash seen images (#17)
    * feat(LazyImage): dont flash seen images
    
    * fix: resolve build problem >_<
    
    * fix: resolve build problem >_< 2

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -1,6 +1,17 @@
 import React, { useRef, useEffect, useReducer } from 'react';
 import styled, { css } from 'styled-components';
 
+enum ImageState {
+  ready,
+  loading,
+  loaded,
+}
+
+const instaShowStyle = css`
+  opacity: 1;
+  transition: unset;
+`;
+
 const unloadedStyle = css`
   width: 100% !important;
   height: 100% !important;
@@ -12,12 +23,23 @@ const loadedStyle = css`
 `;
 
 interface ImageProps {
-  loaded: boolean;
+  loadedState: ImageState;
 }
 
-const Image = styled.img`
+const getImageStyle = ({ loadedState }: ImageProps) => {
+  switch (loadedState) {
+    case ImageState.loading:
+      return unloadedStyle;
+    case ImageState.loaded:
+      return loadedStyle;
+    case ImageState.ready:
+      return instaShowStyle;
+  }
+};
+
+const Image = styled.img<ImageProps>`
   transition: 0.3s opacity linear;
-  ${({ loaded }: ImageProps) => (loaded ? loadedStyle : unloadedStyle)}
+  ${getImageStyle}
 `;
 
 interface Props {
@@ -25,18 +47,43 @@ interface Props {
   alt: string;
 }
 
+const reducerFunction = (src: string) => () => {
+  if (Array.isArray(window.__imagesSeen__)) {
+    if (!window.__imagesSeen__.includes(src)) {
+      window.__imagesSeen__.push(src);
+    }
+  } else {
+    window.__imagesSeen__ = [src];
+  }
+
+  return ImageState.loaded;
+};
+
+const getStatus = (src: string) => {
+  if (typeof window !== 'undefined' && Array.isArray(window.__imagesSeen__) && window.__imagesSeen__.includes(src)) {
+    return ImageState.ready;
+  }
+
+  return ImageState.loading;
+};
+
 const LazyImage: React.FC<Props> = ({ src, alt, ...props }) => {
-  const [loaded, handleLoaded] = useReducer(a => !a, false);
+  const [loaded, handleLoaded] = useReducer(reducerFunction(src), getStatus(src));
+
   const image = useRef<HTMLImageElement>(null);
   const observer = useRef<IntersectionObserver | null>(null);
 
   const setSrc = () => {
-    if (image.current) {
+    if (image.current && !image.current.src) {
       image.current.src = src;
     }
   };
 
   useEffect(() => {
+    if (loaded === ImageState.ready) {
+      return () => undefined;
+    }
+
     observer.current = new IntersectionObserver(entries =>
       entries.forEach(
         ({ isIntersecting }) => {
@@ -69,7 +116,16 @@ const LazyImage: React.FC<Props> = ({ src, alt, ...props }) => {
     };
   }, []);
 
-  return <Image loaded={loaded} onLoad={handleLoaded} ref={image} alt={alt} {...props} />;
+  return (
+    <Image
+      loadedState={loaded}
+      onLoad={handleLoaded}
+      ref={image}
+      alt={alt}
+      src={getStatus(src) === ImageState.ready ? src : undefined}
+      {...props}
+    />
+  );
 };
 
 export default LazyImage;
